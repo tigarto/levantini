cce5cda738ca0ddeb6ca7bffc828ce593ce56c8a
Unknown <helpu1409@gmail.com>
2018-07-18 20:14:40 -0500

    subiendo lo ultimo

--- consola3.c
+++ consola3.c
@@ -19,29 +19,23 @@ gcc -Wall consola3.c -o consola
 int main(int argc, char *argv[])
 {
     //printf("-> P(*)\n");
-    printf("prompt > ./solo");   
     int fd;   // file descriptor 
     int rc = fork();
     if (rc < 0) {
         // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {
-        // printf("-> H\n");
-        // child (new process)
-        /**redirection of I/O*/
-        fd = open("solo.output", O_WRONLY | O_CREAT);
-        close(stdout);  //stdout => 1
-        dup(fd);
-        close(fd);
+    } else if (rc == 0) {  
+        printf("%s\n", argv[1]);      
+        fd = open("./test1", O_WRONLY | O_CREAT);        
         /* stdout is now redirected */        
-        char *param[] = { NULL };
-        execv("./solo", param);        
+        char *param[] = { argv[1],
+                          NULL };
+        printf("%s\n", param[0]);      
+        execv("./test1", param);        
     } else {
         // printf("-> P(**)\n");
         int rc_wait = wait(NULL);
-        // printf("-> P(**)\n");
-        printf("\nprompt >\n");    
     }
     return 0;
 }
